7dde44cf232664f069192804c3c71c2fcd725bda
biswa-m <biswa12jit@gmail.com>
2018-11-06 14:09:58 +0530

    Parse HTTP methods, query strings, headers and payloads

--- pirple_tutorials/up_time_monitor/index.js
+++ pirple_tutorials/up_time_monitor/index.js
@@ -3,6 +3,7 @@
 // Dependancies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
@@ -14,14 +15,41 @@ var server = http.createServer(function(req, res){
 	var path = parsedUrl.pathname;
 	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
-	// Send the response
-	res.end('Hello World\n');
+	// Get the query string as an object
+	var queryStringObject = parsedUrl.query;
 
-	// Log the request path
-	console.log('Request received on path: '+trimmedPath);
+	// Get the HTTP Method
+	var method = req.method.toLowerCase();
+
+	// Get the header as an object
+	var headers = req.headers;
+
+	// Get the payload, if any
+	var decoder = new StringDecoder('utf-8');
+	var buffer = '';
+	req.on('data', function(data){
+		buffer += decoder.write(data);
+	});
+	req.on('end', function(){
+		buffer += decoder.end();
+
+		// Send the response
+		res.end('Hello World\n');
+
+		// Log the request path
+		console.log('Request received on path: ' + trimmedPath + 
+			'\nwith the method: ' + method +
+			'\nwith these query string parameters: ', 
+							queryStringObject,
+			'\nwith these headers: ', headers,
+			'\nwith this payload: ', buffer,
+			'\n');
+
+	});
+	
 });
 
 // Start the server and have it listen on port 3000
 server.listen(3000, function(){
-	console.log("The server is listening in port 3000 now");
+	console.log("The server is listening in port 3000 now\n");
 });
