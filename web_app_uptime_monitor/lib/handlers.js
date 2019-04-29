5d5d7bad618feefd02173660cfc98dd6ffafdd83
Biswajit Mahanta <bis.mhnta@gmail.com>
2019-04-29 22:32:10 +0530

    webAppUptimeMonitor: Edit account

--- web_app_uptime_monitor/lib/handlers.js
+++ web_app_uptime_monitor/lib/handlers.js
@@ -134,6 +134,35 @@ handlers.sessionDeleted = function(data, callback){
 	} 
 };
 
+// Edit your account
+handlers.accountEdit = function(data, callback){
+	// Reject any request that isn't a GET
+	if (data.method == 'get') {
+		// Prepare data for interpolation
+		var templateData = {
+			'head.title' : 'Account Settings',
+			'body.class': 'accountEdit'
+		}
+		// Read in a template as a string
+		helpers.getTemplate('accountEdit', templateData, function(err, str){
+			if (!err && str) {
+				// Add the universal header and footer
+				helpers.addUniversalTemplates(str, templateData, function(err, str) {
+					if (!err && str) {
+						callback(200, str, 'html');
+					} else {
+						callback(500, undefined, 'html');
+					}
+				});
+			} else {
+				callback(500, undefined, 'html');
+			}
+		});
+	} else {
+		callback(405, undefined, 'html');
+	} 
+};
+
 // Favicon
 handlers.favicon = function(data, callback) {
 	// Reject any method that isn't a get
