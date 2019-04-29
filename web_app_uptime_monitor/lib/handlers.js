f188f30554e206cc76146ca98bb600950c0272cd
Biswajit Mahanta <bis.mhnta@gmail.com>
2019-04-29 22:40:43 +0530

    webAppUptimeMonitor: Delete account

--- web_app_uptime_monitor/lib/handlers.js
+++ web_app_uptime_monitor/lib/handlers.js
@@ -163,6 +163,36 @@ handlers.accountEdit = function(data, callback){
 	} 
 };
 
+// Account has been deleted
+handlers.accountDeleted = function(data, callback){
+	// Reject any request that isn't a GET
+	if (data.method == 'get') {
+		// Prepare data for interpolation
+		var templateData = {
+			'head.title' : 'Account Deleted',
+			'head.description' : 'Your account has been deleted',
+			'body.class' : 'accountDeleted'
+		}
+		// Read in a template as a string
+		helpers.getTemplate('accountDeleted', templateData, function(err, str){
+			if (!err && str) {
+				// Add the universal header and footer
+				helpers.addUniversalTemplates(str, templateData, function(err, str) {
+					if (!err && str) {
+						callback(200, str, 'html');
+					} else {
+						callback(500, undefined, 'html');
+					}
+				});
+			} else {
+				callback(500, undefined, 'html');
+			}
+		});
+	} else {
+		callback(405, undefined, 'html');
+	} 
+};
+
 // Favicon
 handlers.favicon = function(data, callback) {
 	// Reject any method that isn't a get
