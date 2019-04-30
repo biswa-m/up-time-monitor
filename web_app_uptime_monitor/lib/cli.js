7f4d5e974de255d38de4a2a09256ce4894695c8b
Biswajit Mahanta <bis.mhnta@gmail.com>
2019-05-01 00:44:11 +0530

    Handle commands: man, help

--- web_app_uptime_monitor/lib/cli.js
+++ web_app_uptime_monitor/lib/cli.js
@@ -50,8 +50,84 @@ cli.responders = {};
 
 // Help / man
 cli.responders.help = function() {
-	console.log('You asked for help');
+	var commands = {
+		'exit': 'Kill the CLI (and rest of the application)',
+		'man': 'Show this help page',
+		'help': 'Alias of the "man" command',
+		'stats': 'Get statisics of the underlying operations and resourse utilisation',
+		'list users': 'Show a list of all the registered (undeleted) user in the system',
+		'more user info --{userId}': 'Show details of a specific user',
+		'list checks --up --down': 'Show a list of all the active checks in the system, including their satate, The "--up" and the "--down" flags are both optional',
+		'more check info --{checkId}': 'Show details of a specified checks',
+		'list logs': 'Show a list of all logs file available to be read (compressed and uncompressed)',
+		'more log info --{fileName}': 'Show details of a specified log file' 
+	};
+
+	// Show a header for the help page that is as wide as the screen
+	cli.horizontalLine();
+	cli.centered('CLI MANUAL');
+	cli.horizontalLine();
+	cli.verticalSpace(2);
+
+	// Show each command, followed by its explanation, in white and yellow respectively
+	for (var key in commands) {
+		if (commands.hasOwnProperty(key)) {
+			var value = commands[key];
+			var line = '\x1b[33m'+key+'\x1b[0m';
+			var padding = 60 - line.length;
+
+			for (i = 0; i < padding; ++i) {
+				line += ' ';
+			}
+			line += value;
+			console.log(line);
+			cli.verticalSpace();
+		}
+	};
+
+	cli.verticalSpace(1);
+	cli.horizontalLine();
 }
+
+// Create a vertical space
+cli.verticalSpace = function(lines) {
+	lines = typeof(lines) == 'number' && lines > 0 ? lines : 1;
+	for (i = 0; i < lines; ++i) {
+		console.log('');
+	}
+};
+
+// Create a horizontal line accross the screen
+cli.horizontalLine = function(){
+	// Get the available screen size
+	var width = process.stdout.columns;
+
+	var line = '';
+	for (i = 0; i < width; ++i) {
+		line += '-';
+	}
+	console.log(line);
+};
+
+// Create centered text on the screen
+cli.centered = function(str) {
+	str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : '';
+
+	// Get the available screen size
+	var width = process.stdout.columns;
+
+	// Calculate the left padding there should be
+	var leftPadding = Math.floor((width - str.length) / 2);
+
+	// Put in left padded spaces before the string itself
+	var line = '';
+	for (i = 0; i < leftPadding; ++i) {
+		line += ' ';
+	};
+	line += str;
+	console.log(line);
+}
+
 cli.responders.exit = function() {
 	process.exit(0);
 }
